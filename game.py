85059ed3dd0896ec5dcee6eb80af259aed4096e4
Cameron Steer <cdsteer94@gmail.com>
2012-06-19 19:41:08 +0100

    updated game.py

--- game.py
+++ game.py
@@ -1,82 +1,84 @@
 import sys
+import player
+from player import * 
+import mob
+from mob import * 
 
 print "Welcome Player, start by entering your name:"
-plrname = raw_input(">")
-print "Hello there %s, \nYou are about to enter a cave of no return." % plrname
+name = raw_input(">")
+print "Hello there %s, \nYou are about to enter a cave of no return." % name
 print "Once inside the only way back a mistic portal at the end." 
 print "So why take this risk? Because within this cave lies the worlds" 
 print "largest collection of tresuse hiden away by a long dead king and" 
-print "prtected by evil monsters, so good luck :D"
+print "protected by evil monsters, so good luck :D"
 
-print "Choise you class:"
-print "1. Warrier: Power = Furry"
+print "Choose you class:"
+print "1. Warrior: Power = Furry"
 print "2. Thief: Power = Poison"
 print "3. Mage: Power = Fireball"
 
 pclass = raw_input(">")
 
 if pclass == "1":
-	pclass = "warrier"
-	classpower = "Furry"
+	#make the player setting the name, hp, dp depending on the class
+	player = Player(name, "Warrior", "Furry", 300, 300, 10)
 elif pclass == "2":
 	pclass = "thief"
 	classpower = "poison"
+	playerhp = Player(200)
 elif pclass == "3":
 	pclass = "mage"
 	classpower = "fireball"
+	playerhp = Player(150)
 
 print "Welcome %s" % pclass
 
-def lifecheck (playerhp):
-	if (playerhp < 1):
-		return "Game Over"
-		sys.exit()
-	else:
-		return True
+def userattack():
+	hp = mob.getmobHP()
+	dam = player.getplayerDP()
+	newhp = hp - dam
+	mob.setmobHP(newhp)
+	print "You deal %r points of damage" % player.getplayerDP()
+	print "%s: %r/%r" % (mob.getmobname(), mob.getmobHP(), mob.getmobmaxHP())
+	return
 
-def userattack(mob, attack, hp):
-	hpnew = hp - attack
-	print "You deal %r points of damage" % attack
-	print "%s: %r/%r" % (mob, hpnew, hp)
-	return hpnew
+def mobattack():
+	hp = player.getplayerHP()
+	dam = mob.getmobDP()
+	newhp = hp - dam
+	player.setplayerHP(newhp)
+	print "%s deals %r points of damage" % (mob.getmobname(), mob.getmobDP())
+	print "%s: %r/%r" % (player.getplayername(), player.getplayerHP(), player.getplayermaxHP())
+	return
+	
+def battle():
 
-def mobattack(mob, attack, plrname, playerhp):
-	hpnew = playerhp - attack
-	print "%s deals %r points of damage" % (mob, attack)
-	print "%s: %r/%r" % (plrname, hpnew, playerhp)
-	return hpnew
-	
-def battle(mob, hp, plrname, playerhp):
-	
-	while hp > 0:
-		lifecheck(playerhp)
+	while mob.getmobHP() > 0:
+		if player.isalive(mob.getmobname()) == False:
+			sys.exit()
 		print "How will you attack?"
 		print "1. Bassic Attack"
-		print "2. %s" % classpower
+		print "2. %s" % player.getplrpower()
 		print "3. Item"
 		print "4. Quit"
 
 		aplayer = raw_input(">")
 		
 		if aplayer == "1":
-			hp = userattack(mob, 30, hp)
-			playerhp = mobattack(mob, 40, plrname, playerhp)
+			mobattack()
+			userattack()
 		elif aplayer == "2":
-			hp = ratbattle(60, hp, playerhp)
-			playerhp = mobattack(mob, 40, plrname, playerhp)
+			print "Coming Soon"
 		elif aplayer == "3":
-			aplayer == "item"
+			print "Coming Soon"
 		elif aplayer == "4":
 			print "You coward!!!"
 			sys.exit()
 	return True
-	
-playerhp = 200
-print "Enter the dark cave then a large rat goes to attack you"
-win = False
-while win == False:
-	
-	win = battle("Rat", 60, plrname, playerhp)
-	
-print "Your beat the rat a won the tresure"
 
+mob = Mob("Large Rat", 60, 60, 40)
+print "You enter the dark cave then a large %s goes to attack you" % mob.getmobname()
+win = False
+while win == False:	
+	win = battle()
+print "You beat the rat and won the tresure"
